6d0049153b4725047f2a24bc167e8872bb43dcec
Vintage <btadamz@gmail.com>
2023-08-14 22:45:31 +0100

    Solidity Pragma.
    Here i did a little change in the solidity Pragma. 
    Move it to a higher version.

--- contract-f4ab0d2d7b.sol
+++ contract-f4ab0d2d7b.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
