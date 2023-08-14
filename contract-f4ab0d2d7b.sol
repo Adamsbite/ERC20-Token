764f5e08ebd520ccc1aed502a231c14147d079c7
Vintage <btadamz@gmail.com>
2023-08-14 22:48:08 +0100

    Updated Pagma

--- contract-f4ab0d2d7b.sol
+++ contract-f4ab0d2d7b.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol",;
 
 contract Bitetoken is ERC20, Ownable {
     constructor() ERC20("Bitetoken", "btk") {}
