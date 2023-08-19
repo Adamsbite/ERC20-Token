447f6f0fd55880769ae5c9548d952eb21d5935d5
Vintage <btadamz@gmail.com>
2023-08-19 23:24:44 +0100

    Update contract-f4ab0d2d7b.sol

--- contract-f4ab0d2d7b.sol
+++ contract-f4ab0d2d7b.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.9.3/access/Ownable.sol",;
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
 
 contract Bitetoken is ERC20, Ownable {
     constructor() ERC20("Bitetoken", "btk") {}
